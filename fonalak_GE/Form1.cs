4fe204b255bf2247a368ee0202143064aee9346c
Gircsi117 <gilianerik1107@gmail.com>
2021-11-17 09:19:28 +0100

    Elemek felvétele

--- fonalak_GE/Form1.cs
+++ fonalak_GE/Form1.cs
@@ -12,6 +12,9 @@ namespace fonalak_GE
 {
     public partial class Form1 : Form
     {
+        static List<Elem> adatok = new List<Elem>() { };
+        static PictureBox[,] tabla = new PictureBox[3,3];
+
         public Form1()
         {
             InitializeComponent();
@@ -24,6 +27,82 @@ namespace fonalak_GE
             alap_general();
         }
 
+        //piros-1, kék-2, zöld-3, sárga-4
+        private void elemgeneral()
+        {
+            adatok.Add(new Elem(new int[4, 4]
+            {
+                {0, 4, 2, 0},
+                {3, 0, 0, 1 },
+                {1, 0, 0, 2 },
+                {0, 4, 3, 0 }
+            }, 0, 0));
+
+            adatok.Add(new Elem(new int[4, 4]
+            {
+                {0, 3, 1, 0},
+                {1, 0, 0, 4 },
+                {2, 0, 0, 2 },
+                {0, 4, 3, 0 }
+            }, 0, 1));
+
+            adatok.Add(new Elem(new int[4, 4]
+            {
+                {0, 1, 3, 0},
+                {4, 0, 0, 2 },
+                {2, 0, 0, 1 },
+                {0, 4, 3, 0 }
+            }, 0, 2));
+
+            adatok.Add(new Elem(new int[4, 4]
+            {
+                {0, 4, 3, 0 },
+                {2, 0, 0, 2 },
+                {3, 0, 0, 1 },
+                {0, 1, 4, 0 }
+            }, 0, 3));
+
+            adatok.Add(new Elem(new int[4, 4]
+            {
+                {0, 4, 3, 0 },
+                {2, 0, 0, 1 },
+                {1, 0, 0, 3 },
+                {0, 4, 2, 0 }
+            }, 0, 4));
+
+            adatok.Add(new Elem(new int[4, 4]
+            {
+                {0, 4, 3, 0 },
+                {1, 0, 0, 1 },
+                {3, 0, 0, 2 },
+                {0, 2, 4, 0 }
+            }, 0, 5));
+
+            adatok.Add(new Elem(new int[4, 4]
+            {
+                {0, 1, 4, 0 },
+                {4, 0, 0, 2 },
+                {3, 0, 0, 3 },
+                {0, 2, 1, 0 }
+            }, 0, 6));
+
+            adatok.Add(new Elem(new int[4, 4]
+            {
+                {0, 4, 2, 0 },
+                {2, 0, 0, 1 },
+                {3, 0, 0, 3 },
+                {0, 1, 4, 0 }
+            }, 0, 7));
+
+            adatok.Add(new Elem(new int[4, 4]
+            {
+                {0, 2, 4, 0 },
+                {1, 0, 0, 3 },
+                {3, 0, 0, 2 },
+                {0, 1, 4, 0 }
+            }, 0, 8));
+        }
+
         private void alap_general()
         {
             alapPANEL.Controls.Clear();
@@ -31,6 +110,8 @@ namespace fonalak_GE
 
             int x = 12;
             int y = 12;
+
+            int kepszam = 0;
 
             for (int i = 0; i < 3; i++)
             {
@@ -42,16 +123,33 @@ namespace fonalak_GE
                     pn.BorderStyle = BorderStyle.FixedSingle;
                     alapPANEL.Controls.Add(pn);
                     pn.Location = new Point(x, y);
-                    pn.Image = kepekIMAGELIST.Images[0];
+                    pn.Image = kepekIMAGELIST.Images[kepszam];
                     pn.SizeMode = PictureBoxSizeMode.Zoom;
 
+                    tabla[i, j] = pn;
+
+                    kepszam++;
                     x += szeles;
                 }
                 x = 12;
                 y += szeles;
             }
+            
+        }
 
-            //(alapPANEL.Controls[0] as PictureBox).Image.RotateFlip(RotateFlipType.Rotate180FlipNone);
+        private void kepekberak()
+        {
+            int sorszam = 0;
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    tabla[i, j].Image = kepekIMAGELIST.Images[adatok[sorszam].Kepszam];
+
+                    sorszam++;        
+                }
+            }
         }
     }
 }
